b7343e23407f78d27bca3231ed32fe51ee034884
Seven <1319129926@qq.com>
2022-08-24 16:05:09 +0800

    ver.day03.task

---
+++ src/01-tesk.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="reverseFn">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ["美女", "程序猿", "帅哥"],
+    };
+  },
+  methods: {
+    reverseFn(){
+      this.arr.push(this.arr[0])
+      this.arr.shift()
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ src/02-task.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in myArr" :key="index">
+        {{ item }}
+        <button @click="delFn(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="addFn">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myArr: [1, 5, 3],
+    };
+  },
+  methods: {
+    addFn() {
+      this.myArr.push(Math.floor(Math.random() * 20) + 1);
+    },
+    delFn(index) {
+      this.myArr.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
